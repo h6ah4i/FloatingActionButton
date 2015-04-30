9d818a2ca0e5be12a52b5e6ded123436db95ccaa
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-04-30 16:05:22 +0900

    Add FloatingActionMenu.getMenuButton() method

--- library/src/main/java/com/github/clans/fab/FloatingActionMenu.java
+++ library/src/main/java/com/github/clans/fab/FloatingActionMenu.java
@@ -835,4 +835,8 @@ public class FloatingActionMenu extends ViewGroup {
         removeView(fab);
         mButtonsCount--;
     }
+
+    public FloatingActionButton getMenuButton() {
+        return mMenuButton;
+    }
 }
